11bab05f810b13bedbc1bbe09d9fe98cc07dc422
zhangwei123 <1551583950@qq.com>
2016-11-19 18:47:34 +0800

    貌似解决了图片显示错位的状况，需要后期学习

--- app/src/main/java/com/play/zv/seamountain/GrilAdapter.java
+++ app/src/main/java/com/play/zv/seamountain/GrilAdapter.java
@@ -4,16 +4,12 @@ import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.animation.ObjectAnimator;
 import android.content.Context;
-import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.squareup.picasso.Picasso;
 
 import GrilInfo.GrilInfo;
@@ -82,9 +78,11 @@ public class GrilAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
             Picasso.with(mContext)
                     .load(grilInfo.getResults().get(position).getUrl())
-                    .placeholder(mContext.getDrawable(R.mipmap.ic_launcher))//没有加载图片时显示的默认图像
+                    .placeholder(R.drawable.pic_loading_round)//没有加载图片时显示的默认图像
+                    .resize(480,720)
+                    .centerCrop()
                     .error(mContext.getDrawable(R.mipmap.ic_launcher))
-                    .into(((MyViewHolder) holder).iv);// 被加载的控件
+                    .into(((MyViewHolder) holder).iv);// 被f加载的控件
         }
 
 
