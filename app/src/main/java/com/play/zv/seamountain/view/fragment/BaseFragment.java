e679ec64b0a0879aae394047dd042219ccb188d1
zhangwei123 <1551583950@qq.com>
2016-12-01 23:13:42 +0800

    准备Fragment！

---
+++ app/src/main/java/com/play/zv/seamountain/view/fragment/BaseFragment.java
@@ -0,0 +1,38 @@
+package com.play.zv.seamountain.view.fragment;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Zv on 2016/12/01.
+ */
+
+public abstract class BaseFragment extends Fragment {
+    public Activity mActivity;
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mActivity=getActivity();
+    }
+    //处理Fragment的布局
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return initViews();
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        initData();
+    }
+    public abstract View initViews();
+    public void  initData(){
+
+    }
+}
